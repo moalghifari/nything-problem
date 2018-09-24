b017865aec5e71435b91e0b2e31a3c6e18030e57
M. Rafli Al Khadafi <raflyalk@gmail.com>
2018-09-24 16:27:17 +0700

    Done Genetic Algorithm (hopefully works) bismillah huhuhu

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -1,43 +1,81 @@
-from State import State
 from numpy import random
+from math import ceil
+from copy import deepcopy
+
+from State import State
 
 POPULATION_NUMBER = 10
-MUTATION_RATE = 0.1
+MUTATION_RATE = 0.01
+
+
+def main(pawnInput):
+  population = generateListOfRandomPopulation(POPULATION_NUMBER, pawnInput)
+  return solve(population)
 
 # totalPopulationHeuristic: sum of all heuristic of all states in population
 # fitness function = 1 - heuristic / totalPopulationHeuristic
 
-def solve(pop=[], pawnInput=[]):
+def solve(population):
   totalPopulationHeuristic = 0
   fitness = []
+  childrenPopulation = []
 
-  if not pop and not pawnInput:
-    raise Exception('Cannot solve nything problem using genetic algorithm without pawn information')
-  elif pop:
-    population = pop
-  else:
-    population = generateListOfRandomPopulation(POPULATION_NUMBER, pawnInput)
+  # check if there is a state with the best heuristic, if there is one, then return the state
+  for individual in population:
+    if individual.totalHeuristic == 0:
+      return individual
 
   for individual in population:
-    totalPopulationHeuristic += individual.getTotalHeuristic()
+    totalPopulationHeuristic += individual.totalHeuristic
 
-  # counting fitness function for each individual
+  # calculate fitness function for each individual
   for individual in population:
     fitness.append(1 - float(individual.getTotalHeuristic()) / totalPopulationHeuristic)
 
-  for i in POPULATION_NUMBER:
-    
-
-
-
+  for _ in range(int(ceil(POPULATION_NUMBER/2.0))):
+    # choose 2 parents randomly based on the fitness function
+    parent1 = random.choice(population, p=fitness)
+    while True:
+      parent2 = random.choice(population, p=fitness)
+      if parent2 != parent1:
+        break
+    # crossover those 2 parents, concate the children to childrenPopulation
+    childrenPopulation += crossOver(parent1, parent2)
   
+  # If there is an odd number of population -> generate excess one children and needs to be removed
+  if len(childrenPopulation) > POPULATION_NUMBER:
+    childrenPopulation.pop()
 
-
+  return solve(childrenPopulation)
 
 def generateListOfRandomPopulation(populationNumber, pawnInput):
   population = []
-  for i in range(populationNumber):
+  for _ in range(populationNumber):
     population.append(State(pawnInput=pawnInput))
   return population
 
-def crossover(state1, state2):
+def crossOver(state1, state2):
+  children = []
+  pawnCount = len(state1.listOfPawn)
+
+  pivot = random.choice(range(pawnCount))
+
+  listOfPawn1 = deepcopy(state1.listOfPawn[:pivot] + state2.listOfPawn[pivot:])
+  listOfPawn2 = deepcopy(state2.listOfPawn[:pivot] + state1.listOfPawn[pivot:])
+
+  children.append(mutate(listOfPawn1))
+  children.append(mutate(listOfPawn2))
+  
+def mutate(listOfPawn):
+  if random.choice([True, False], p=[MUTATION_RATE, 1-MUTATION_RATE]):
+    index = random.choice(range(len(listOfPawn)))
+    while True:
+      x = random.choice(range(8))
+      y = random.choice(range(8))
+      if not filter(lambda pawn: pawn.x == x and pawn.y == y, listOfPawn):
+        break
+    listOfPawn[index].x = x
+    listOfPawn[index].y = y
+    return State(listOfPawn=listOfPawn)
+  else:
+    return State(listOfPawn=listOfPawn)

9fd71dabce2fc46ab087f33efad181b4823f9337
Sekar <annisayuningtyas28@yahoo.com>
2018-09-26 16:10:35 +0700

    Fixed

--- Pawn.py
+++ Pawn.py
@@ -1,34 +1,22 @@
 from constants import WHITE_PAWN, BLACK_PAWN
-from State import State
 
 
 class Pawn:
-    def __init__(self, currentPawn) :
-        """
-            This is a constructor
-            currentPawn is a dictionary
-        """
-        self.type = currentPawn["pawnType"]
-        self.x = currentPawn["x"]
-        self.y = currentPawn["y"]
+    def __init__(self, type, x, y) :
+        self.type = type
+        self.x = x
+        self.y = y
         self.listOfPossibleMove = []
         self.sameColorHeuristic = 0
         self.diffColorHeuristic = 0
 
     def outChessBoard(self, x, y) :
-        """
-            return True if a point is outside the chess board
-        """
         if (x < 0 or x > 7 or y < 0 or y > 7) :
             return True
         else :
             return False
 
     def checkHorizontal(self) :
-        """
-            return possible move points of pawn by checking from horizontal and vertical side
-            use for Rook and Queen
-        """
         PossibleMove = []
         # check right
         for i in range(1,8) :
@@ -56,10 +44,6 @@ class Pawn:
                 PossibleMove.append([self.x,self.y-i])
         return PossibleMove
     def checkDiagonal(self) :
-        """
-            return possible move points of pawn by checking from diagonal side
-            use for Bishop and Queen
-        """
         PossibleMove = []
         # check diagonal down right
         for i in range(1,8) :
@@ -88,10 +72,6 @@ class Pawn:
         return PossibleMove
 
     def checkHorizontalHeuristic(self, pawn_color, Board) :
-        """
-            return heuristic of pawn by checking from horizontal and vertical side
-            use for Rook and Queen
-        """
         heuristic = 0
         # horizontal check
         for n in range(1,8) :
@@ -113,10 +93,6 @@ class Pawn:
                 break
         return heuristic
     def checkDiagonalHeuristic(self, pawn_color, Board) :
-        """
-            return heuristic of pawn by checking from diagonal side
-            use for Bishop and Queen
-        """
         heuristic = 0
         # diagonal down right check
         for n in range(1,8) :
@@ -141,9 +117,6 @@ class Pawn:
         return heuristic
 
     def generatePossibleMoves(self, chessBoard) :
-        """
-            return possible move points of pawn based on the type
-        """
         if (self.type == 'K' or self.type == 'k') :
             if (not(self.outChessBoard(self.x+1, self.y+2))) : self.listOfPossibleMove.append([self.x+1, self.y+2])
             if (not(self.outChessBoard(self.x+2, self.y+1))) : self.listOfPossibleMove.append([self.x+2, self.y+1])
@@ -163,10 +136,6 @@ class Pawn:
         return self.listOfPossibleMove
 
     def calcHeuristic(self, chessBoard, pawn_color) :
-        """
-            return heuristic of pawn based on the type
-            heuristic is the number of pawn that can be attacked by itself
-        """
         heuristic = 0
         if (self.type == 'K' or self.type == 'k') :
             if (not(self.outChessBoard(self.x+1, self.y-2)) and chessBoard[self.y-2][self.x+1] in pawn_color) :
@@ -194,11 +163,6 @@ class Pawn:
         return heuristic
 
     def calcSameColorHeuristic(self, chessBoard) :
-        """
-            return the same color pawn heuristic
-            if black pawn then search for black pawn
-            if white pawn then search for white pawn
-        """
         if (self.type == 'K' or self.type == 'Q' or self.type == 'B' or self.type == 'R' ) :
             pawn_color = WHITE_PAWN
         else :
@@ -206,11 +170,6 @@ class Pawn:
         return self.calcHeuristic(chessBoard, pawn_color)
 
     def calcDiffColorHeuristic(self, chessBoard) :
-        """
-            return the different color pawn heuristic
-            if black pawn then search for white pawn
-            if white pawn then search for black pawn
-        """
         if (self.type == 'K' or self.type == 'Q' or self.type == 'B' or self.type == 'R' ) :
             pawn_color = BLACK_PAWN
         else :
@@ -219,32 +178,22 @@ class Pawn:
 
 
 def main() :
-    pawnInput = [
-        {
-            'pawnType': 'K',
-            'pawnCount': 2
-        }, {
-            'pawnType':'B',
-            'pawnCount': 2
-        }, {
-            'pawnType':'R',
-            'pawnCount': 2
-        }, {
-            'pawnType':'Q',
-            'pawnCount': 2
-        }, {
-            'pawnType':'q',
-            'pawnCount': 2
-        }
+    CHESS_BOARD = [
+        ['Q','.','.','.','.','Q','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','B','.','K','.','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','Q','.','.','.','k','.'],
+        ['.','B','.','.','R','.','.','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','.','.','.','.','.','.']
     ]
-    state = State(pawnInput=pawnInput)
-    a = Pawn(state.listOfPawn[6])
-    n = a.generatePossibleMoves(state.chessBoard)
-    j = a.calcSameColorHeuristic(state.chessBoard)
-    print("this pawn = ", state.listOfPawn[6])
+    a = Pawn("K", 4, 2)
+    n = a.generatePossibleMoves(CHESS_BOARD)
+    j = a.calcSameColorHeuristic(CHESS_BOARD)
+    print("this pawn = ", a.type, a.x, a.y)
     print("possible moves = ", n)
     print("same color heuristic = ", j)
-    state.printChessBoard()
 
 
 if __name__ == '__main__':

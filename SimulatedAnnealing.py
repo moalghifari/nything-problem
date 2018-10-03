d91e90900b294c360347677cba036347594f35cb
Unknown <annisayuningtyas28@yahoo.com>
2018-10-03 14:59:12 +0700

    Added commenting

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -10,6 +10,16 @@ from sys import stdout
 
 
 def solve(state, type, temperatur, value) :
+    """
+        solve n-ything problem with Simulated Annealing Algorithm
+        1. If total cost is equal to 0 then solved
+        2. Else, tempSolution is currentState
+        3. Get candidate of next state
+        4. Generate new solution by randomly choose one in candidate of next state
+        5. If new solution's total cost is lower than current cost, then go to number 2
+        6. Else, depend on a function will determine whether tempSolution or new solution to be the new tempSolution
+        7. Repeat number 2 until 6 until 1000 times
+    """
     if (state.totalCost == 0):
         return state
     tempSolution = deepcopy(state)
@@ -26,6 +36,12 @@ def solve(state, type, temperatur, value) :
             probability = numpy.exp(-delta/temperatur)
             if random.choice([True,False], p=[probability, 1-probability]):
                 tempSolution = newSolution
+        """
+            Type of temperature change :
+            type 1 : Constant
+            type 2 : Linear
+            type 3 : Logaritmic
+        """
         if (type == '1') :
             temperatur = temperatur
         elif (type == '2') :
@@ -37,6 +53,9 @@ def solve(state, type, temperatur, value) :
     return tempSolution
 
 def main(pawnInput):
+    """
+        main function of Simulated Annealing
+    """
     typeF = 3
     temperatur = 10
     value = 0.2

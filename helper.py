ee857fee2680ee27dbd3befbff31aa67b9a67018
Unknown <annisayuningtyas28@yahoo.com>
2018-10-03 13:35:15 +0700

    Added commenting

--- helper.py
+++ helper.py
@@ -2,6 +2,10 @@ from copy import deepcopy
 from State import State
 
 def getListPossibleState(state) :
+    """
+        return all possible next state for each pawn in current state
+        possible next state for each pawn is all point that not filled with pawn
+    """
     listState = []
     tempState = deepcopy(state)
     for pawn in tempState.listOfPawn :
@@ -16,6 +20,9 @@ def getListPossibleState(state) :
 
 
 def getStateCostMin(ListPossibleState) :
+    """
+        return the state with minimum cost
+    """
     stateMin = ListPossibleState[0]
     for unitState in ListPossibleState :
         if unitState.totalCost < stateMin.totalCost :
--- hillClimbing.py
+++ hillClimbing.py
@@ -4,6 +4,14 @@ from sys import stdout
 
 
 def solve(currentState):
+    """
+        solve n-ything problem with hillClimbing Algorithm
+        1. If total cost is equal to 0 then solved
+        2. Else, get candidate of next state
+        3. Search for minimal cost state
+        4. If no minimal cost state lower than current cost, then solved
+        5. Else, back to number 2
+    """
     stdout.write('\rCost terbaik: {} {}'.format(currentState.sameColorCost, currentState.diffColorCost))
     if (currentState.totalCost == 0):
         return currentState
